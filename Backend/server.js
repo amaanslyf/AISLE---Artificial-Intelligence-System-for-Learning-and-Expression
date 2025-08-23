fe4e60c8a060336a7836e22c439d3c68f6931347
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-08-23 13:07:12 +0530

    added routes to another file and included it here

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js"
 
 
 const app =express();
@@ -9,6 +10,8 @@ const PORT=process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api",chatRoutes);
 
 app.listen(PORT,()=>{
   console.log(`Server running on port ${PORT}`);
