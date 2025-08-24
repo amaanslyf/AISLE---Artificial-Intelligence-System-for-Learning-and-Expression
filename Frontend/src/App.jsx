d9e51a64d0bdb462fd793f4480dab9f1827a0820
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-08-24 11:46:45 +0530

    created state variables and added main display pages

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Sidebar from './Sidebar.jsx'
+import ChatWindow from './ChatWindow.jsx'
+import { MyContext } from './MyContext.jsx'
+import { useState } from 'react'
+import {v1 as uuidv1} from 'uuid'
+
+function App() {
+  const [prompt, setPrompt] = useState('');
+  const [reply, setReply] = useState('');
+  const [currThreadId, setCurrThreadId] = useState(uuidv1());
+  const providerValues = {prompt, setPrompt, reply, setReply, currThreadId, setCurrThreadId};
+  
+  return (
+    <div className='app'>
+      <MyContext.Provider value={providerValues}>
+      <Sidebar></Sidebar>
+      <ChatWindow></ChatWindow>
+      </MyContext.Provider>
+    </div>
+  )
+}
+
+export default App
