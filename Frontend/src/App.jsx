56ed916ae66b5c120255d50a76585186394eff0f
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-08-25 09:37:49 +0530

    added more state variables

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,19 +3,23 @@ import Sidebar from './Sidebar.jsx'
 import ChatWindow from './ChatWindow.jsx'
 import { MyContext } from './MyContext.jsx'
 import { useState } from 'react'
-import {v1 as uuidv1} from 'uuid'
+import { v1 as uuidv1 } from 'uuid'
 
 function App() {
   const [prompt, setPrompt] = useState('');
   const [reply, setReply] = useState('');
   const [currThreadId, setCurrThreadId] = useState(uuidv1());
-  const providerValues = {prompt, setPrompt, reply, setReply, currThreadId, setCurrThreadId};
-  
+  const [prevChats, setPrevChats] = useState([]);
+  const [newChat, setNewChat] = useState(true);
+  const [allThreads, setAllThreads] = useState([]);
+
+  const providerValues = { prompt, setPrompt, reply, setReply, currThreadId, setCurrThreadId, prevChats, setPrevChats, newChat, setNewChat, allThreads, setAllThreads };
+
   return (
     <div className='app'>
       <MyContext.Provider value={providerValues}>
-      <Sidebar></Sidebar>
-      <ChatWindow></ChatWindow>
+        <Sidebar></Sidebar>
+        <ChatWindow></ChatWindow>
       </MyContext.Provider>
     </div>
   )
