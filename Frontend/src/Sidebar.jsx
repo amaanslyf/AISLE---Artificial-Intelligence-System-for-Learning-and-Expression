c07c98876902cb8cc73f68bbb35b730613d13863
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-08-25 09:40:31 +0530

    added delete , change thread, create new chat functionality along with its ui components

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,10 +1,73 @@
 import "./Sidebar.css";
+import { useContext, useEffect } from "react";
+import { MyContext } from "./MyContext.jsx";
+import {v1 as uuidv1} from "uuid";
 
 function Sidebar() {
+    const {allThreads, setAllThreads, currThreadId, setNewChat, setPrompt, setReply, setCurrThreadId, setPrevChats} = useContext(MyContext);
+
+    const getAllThreads = async () => {
+        try {
+            const response = await fetch("http://localhost:8080/api/thread");
+            const res = await response.json();
+            const filteredData = res.map(thread => ({threadId: thread.threadId, title: thread.title}));
+            //console.log(filteredData);
+            setAllThreads(filteredData);
+        } catch(err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getAllThreads();
+    }, [currThreadId])
+
+
+    const createNewChat = () => {
+        setNewChat(true);
+        setPrompt("");
+        setReply(null);
+        setCurrThreadId(uuidv1());
+        setPrevChats([]);
+    }
+
+    const changeThread = async (newThreadId) => {
+        setCurrThreadId(newThreadId);
+
+        try {
+            const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+            const res = await response.json();
+            console.log(res);
+            setPrevChats(res);
+            setNewChat(false);
+            setReply(null);
+        } catch(err) {
+            console.log(err);
+        }
+    }   
+
+    const deleteThread = async (threadId) => {
+        try {
+            const response = await fetch(`http://localhost:8080/api/thread/${threadId}`, {method: "DELETE"});
+            const res = await response.json();
+            console.log(res);
+
+            //updated threads re-render
+            setAllThreads(prev => prev.filter(thread => thread.threadId !== threadId));
+
+            if(threadId === currThreadId) {
+                createNewChat();
+            }
+
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
     return (
-    <section className="sidebar">
+        <section className="sidebar">
             <button onClick={createNewChat}>
-                <img src="src/assets/AISLE.png" alt="AISLE logo" className="logo"></img>
+                New Chat
                 <span><i className="fa-solid fa-pen-to-square"></i></span>
             </button>
 
@@ -16,7 +79,7 @@ function Sidebar() {
                             onClick={(e) => changeThread(thread.threadId)}
                             className={thread.threadId === currThreadId ? "highlighted": " "}
                         >
-                            {thread.title}
+                            {thread.title.length > 20 ? thread.title.slice(0, 20) + "..." : thread.title}
                             <i className="fa-solid fa-trash"
                                 onClick={(e) => {
                                     e.stopPropagation(); //stop event bubbling
@@ -29,7 +92,7 @@ function Sidebar() {
             </ul>
  
             <div className="sign">
-                <p>By Amaan &hearts;</p>
+                <p>With Love from Amaan &hearts;</p>
             </div>
         </section>
     )
