c94b5f27445f503f093a66a352dcb908132f3df1
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-08-25 10:56:33 +0530

    fixed api route

--- Backend/server.js
+++ Backend/server.js
@@ -28,28 +28,3 @@ const connectDB=async()=>{
 }
 
 
-// app.post("/test",async(req,res)=>{
-//   const options={
-//     method:"POST",
-//     headers:{
-//       "Content-Type":"application/json",
-//       "Authorization":`Bearer ${process.env.GROQ_API_KEY}`
-//     },
-//     body:JSON.stringify({
-//       model:"openai/gpt-oss-20b",
-//       messages:[{
-//         role:"user",
-//         content:req.body.message,
-//       }]
-//     })
-//   }  
-//   try{
-//     const response = await fetch("https://api.groq.com/openai/v1/chat/completions",options);
-//     const data=await response.json();
-//     //console.log(data);
-//     res.send(data.choices[0].message.content);
-//   }catch(error){
-//     console.log(error);
-//   }
-  
-// })
