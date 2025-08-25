f828ba04aadf0a33edfee5a07847a159779672bb
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-08-25 11:46:13 +0530

    fixed bug

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -5,11 +5,10 @@ import {v1 as uuidv1} from "uuid";
 
 function Sidebar() {
     const {allThreads, setAllThreads, currThreadId, setNewChat, setPrompt, setReply, setCurrThreadId, setPrevChats} = useContext(MyContext);
-
+    const API_URL = import.meta.env.VITE_API_URL;
+    // const localhost = "http://localhost:8080"
     const getAllThreads = async () => {
         try {
-            const API_URL = import.meta.env.VITE_API_URL;
-            // const localhost = "http://localhost:8080"
             const response = await fetch(`${API_URL}/api/thread`);
             const res = await response.json();
             const filteredData = res.map(thread => ({threadId: thread.threadId, title: thread.title}));
